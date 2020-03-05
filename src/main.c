33fe78f1e7f441b61900838b0f4b55cb0de3029c
Alkirys <nik_manz@mail.ru>
2020-03-05 22:27:12 +0300

    fix travis

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ int main() {
     str_vec[1] = (char*)malloc(8* sizeof(char));
     str = (char*)"mail.ru";
     str_vec[1] = (char*)memcpy(str_vec[1], str, 8);
-    
+
     size_t res_vec_len = strings_filter(str_vec, vec_len, &res_str_vec);
     if (res_vec_len == -1){
         return -1;
