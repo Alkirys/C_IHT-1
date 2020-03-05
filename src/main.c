b1cc1d11dd71d0d236f5284566ee7aa0471f35da
Alkirys <nik_manz@mail.ru>
2020-03-05 23:55:21 +0300

    fix main

--- src/main.c
+++ src/main.c
@@ -9,16 +9,17 @@ int main() {
     size_t vec_len = 2;
     //str_vec = input_str_vec(str_vec, &vec_len);
     //if (str_vec == NULL){
-        //return -2;
+    //return -2;
     //}
 
     str_vec = (char **) malloc(vec_len * sizeof(char *));
     str_vec[0] = (char*)malloc(13* sizeof(char));
     char* str = (char*)"Hello world!";
     str_vec[0] = (char*)memcpy(str_vec[0], str, 13);
-    str_vec[0] = (char*)malloc(20* sizeof(char));
-    str = (char*)"http://park.mail.ru/";
-    str_vec[0] = (char*)memcpy(str_vec[0], str, 20);
+
+    str_vec[1] = (char*)malloc(8* sizeof(char));
+    str = (char*)"mail.ru";
+    str_vec[1] = (char*)memcpy(str_vec[1], str, 8);
 
 
     size_t res_vec_len = strings_filter(str_vec, vec_len, &res_str_vec);
