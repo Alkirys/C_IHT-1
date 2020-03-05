cc60a7cd3259b03dd43c1eb90e22cb40ff2b7056
Alkirys <nik_manz@mail.ru>
2020-03-05 22:00:45 +0300

    Update main.c to test travis

--- src/main.c
+++ src/main.c
@@ -6,12 +6,21 @@ int main() {
     char** str_vec = NULL;
     char** res_str_vec = NULL;
 
-    size_t vec_len = 0;
-    str_vec = input_str_vec(str_vec, &vec_len);
-    if (str_vec == NULL){
-        return -2;
-    }
+    size_t vec_len = 2;
+    //str_vec = input_str_vec(str_vec, &vec_len);
+    //if (str_vec == NULL){
+        //return -2;
+    //}
 
+    str_vec = (char **) malloc(vec_len * sizeof(char *));
+    str_vec[0] = (char*)malloc(13* sizeof(char));
+    char* str = (char*)"Hello world!";
+    str_vec[0] = (char*)memcpy(str_vec[0], str, 13);
+
+    str_vec[1] = (char*)malloc(8* sizeof(char));
+    str = (char*)"mail.ru";
+    str_vec[1] = (char*)memcpy(str_vec[1], str, 8);
+    
     size_t res_vec_len = strings_filter(str_vec, vec_len, &res_str_vec);
     if (res_vec_len == -1){
         return -1;
